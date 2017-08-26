6a0dbb0ff932b0a8f2631c3c271e8dcbfbdd8a4c
alayor <alayor3@gmail.com>
2017-08-26 09:47:59 -0500

    Adding support to discover database service and use new greatapp features.

--- src/main/java/com/monederobingo/authorization/authorization/AuthorizationConfiguration.java
+++ src/main/java/com/monederobingo/authorization/authorization/AuthorizationConfiguration.java
@@ -1,10 +1,14 @@
 package com.monederobingo.authorization.authorization;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import xyz.greatapp.libs.service.context.ThreadContextService;
+import xyz.greatapp.libs.service.context.ThreadContextServiceImpl;
+import xyz.greatapp.libs.service.filters.ContextFilter;
 
 @Configuration
 public class AuthorizationConfiguration extends GlobalAuthenticationConfigurerAdapter
@@ -16,5 +20,15 @@ public class AuthorizationConfiguration extends GlobalAuthenticationConfigurerAd
     public void init(AuthenticationManagerBuilder auth) throws Exception
     {
         auth.authenticationProvider(authenticationProvider);
+    }
+
+    @Bean
+    public ThreadContextService getThreadContextService() {
+        return new ThreadContextServiceImpl();
+    }
+
+    @Bean
+    public ContextFilter getContextFilter() {
+        return new ContextFilter(getThreadContextService());
     }
 }
--- src/main/java/com/monederobingo/authorization/authorization/CustomAuthenticationProvider.java
+++ src/main/java/com/monederobingo/authorization/authorization/CustomAuthenticationProvider.java
@@ -1,11 +1,9 @@
 package com.monederobingo.authorization.authorization;
 
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
-import netscape.javascript.JSObject;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -19,64 +17,81 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import xyz.greatapp.libs.service.context.ThreadContextService;
+import xyz.greatapp.libs.service.requests.database.ColumnValue;
+import xyz.greatapp.libs.service.requests.database.SelectQueryRQ;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
-public class CustomAuthenticationProvider implements AuthenticationProvider
-{
+public class CustomAuthenticationProvider implements AuthenticationProvider {
 
-    @Override public Authentication authenticate(Authentication authentication) throws AuthenticationException
-    {
+    private final EurekaClient eurekaClient;
+    private final ThreadContextService threadContextService;
+
+    @Autowired
+    public CustomAuthenticationProvider(EurekaClient eurekaClient,
+                                        ThreadContextService threadContextService) {
+        this.eurekaClient = eurekaClient;
+        this.threadContextService = threadContextService;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String name = authentication.getName();
         String password = authentication.getCredentials().toString();
         CompanyUser user = new CompanyUser();
-        try
-        {
+        try {
             user = getUser(name, password);
-        }
-        catch (SQLException e)
-        {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
-        if (user.getEmail() == null)
-        {
+        if (user.getEmail() == null) {
             throw new BadCredentialsException("BadCredentialsException");
         }
         return new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword(), new ArrayList<>());
     }
 
-    private CompanyUser getUser(String email, String password) throws SQLException
-    {
-        final String DATABASE_SERVICE_URL = "http://test.localhost:30001/";
+    private CompanyUser getUser(String email, String password) throws SQLException {
+        ColumnValue[] filters = new ColumnValue[]{
+                new ColumnValue("email", email),
+                new ColumnValue("password", password)
+        };
 
-        String sql = "SELECT company_user.* FROM company_user" +
-                " WHERE company_user.email = '"+ email + "'" +
-                " AND company_user.password = '" + password + "';";
-
-        HttpEntity<SelectQuery> entity = new HttpEntity<>(
-                new SelectQuery(sql),
+        HttpEntity<SelectQueryRQ> entity = new HttpEntity<>(
+                new SelectQueryRQ("company_user", filters),
                 getHttpHeaders());
         ResponseEntity<DatabaseServiceResult> responseEntity = getRestTemplate().postForEntity(
-                DATABASE_SERVICE_URL + "/select",
+                getDatabaseURL() + "/select",
                 entity,
                 DatabaseServiceResult.class);
 
         Object object = responseEntity.getBody().getObject();
-        if(object == null)
-        {
+        if (object == null) {
             return new CompanyUser();
         }
         return buildCompanyUser(new JSONObject(object.toString()));
     }
 
-    private HttpHeaders getHttpHeaders()
-    {
+    private String getDatabaseURL() {
+        InstanceInfo instanceInfo = eurekaClient.getNextServerFromEureka("database", false);
+        String homePageUrl = instanceInfo.getHomePageUrl();
+        boolean hasHttps = homePageUrl.contains("https://");
+        homePageUrl = homePageUrl.replace("http://", "");
+        homePageUrl = homePageUrl.replace("https://", "");
+        homePageUrl = threadContextService.getThreadContext().getEnvironment().getURIPrefix() + homePageUrl;
+        return hasHttps ? "https://" + homePageUrl : "http://" + homePageUrl;
+    }
+
+    private HttpHeaders getHttpHeaders() {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         return headers;
     }
 
-    private RestTemplate getRestTemplate()
-    {
+    private RestTemplate getRestTemplate() {
         RestTemplate restTemplate = new RestTemplate();
         List<HttpMessageConverter<?>> list = new ArrayList<>();
         list.add(new MappingJackson2HttpMessageConverter());
@@ -84,14 +99,13 @@ public class CustomAuthenticationProvider implements AuthenticationProvider
         return restTemplate;
     }
 
-    @Override public boolean supports(Class<?> authentication)
-    {
+    @Override
+    public boolean supports(Class<?> authentication) {
         return authentication.equals(
                 UsernamePasswordAuthenticationToken.class);
     }
 
-    private CompanyUser buildCompanyUser(JSONObject object) throws SQLException
-    {
+    private CompanyUser buildCompanyUser(JSONObject object) throws SQLException {
         CompanyUser companyUser = new CompanyUser();
         companyUser.setCompanyUserId(object.getLong("company_user_id"));
         companyUser.setCompanyId(object.getLong("company_id"));
